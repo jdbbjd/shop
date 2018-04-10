d1ea3052a879470a562a316bddcfaa3f928a390d
OjieOyuO <793500743@qq.com>
2018-04-10 15:50:41 +0800

    更新home.js/home.css

--- components/home.js
+++ components/home.js
@@ -2,8 +2,7 @@ import React from "react"
 
 import "../assets/css/home.css"
 import 'antd/dist/antd.css';
-import {Carousel,Icon} from 'antd';
-
+import {Carousel,Icon,Button} from 'antd';
 
 
 class Home extends React.Component{
@@ -17,16 +16,26 @@ class Home extends React.Component{
 
 				<div id="nav">
 					<ul>
-						<li>
-							<span>全部分类</span>
-							<ul>
-								<li><Icon type="codepen" /><span>点心/蛋糕</span><Icon type="right" /></li>
-							</ul>
-						</li>
-						<li>首页</li>
-						<li>闪购</li>
-						<li>限时购</li>
-						<li>团购</li>
+						<li>全部分类</li>						
+						<li><a href="#">首页</a></li>
+						<li><a href="#">闪购</a></li>
+						<li><a href="#">限时购</a></li>
+						<li><a href="#">团购</a></li>
+						<li><a href="#">大包装</a></li>
+						<ul>
+							<li><Icon type="smile" /><span>点心/蛋糕</span><Icon type="right" /></li>
+							<li><Icon type="codepen" /><span>饼干/膨化</span><Icon type="right" /></li>
+							<li><Icon type="database" /><span>熟食/肉类</span><Icon type="right" /></li>
+							<li><Icon type="database" /><span>坚果/炒货</span><Icon type="right" /></li>
+							<li><Icon type="gift" /><span>点心/蛋糕</span><Icon type="right" /></li>
+							<li><Icon type="codepen" /><span>熟食/肉类</span><Icon type="right" /></li>
+							<li><Icon type="api" /><span>坚果/炒货</span><Icon type="right" /></li>
+							<li><Icon type="dashboard" /><span>饼干/膨化</span><Icon type="right" /></li>
+							<li><Icon type="github" /><span>饼干/膨化</span><Icon type="right" /></li>
+							<li><Icon type="slack" /><span>饼干/膨化</span><Icon type="right" /></li>
+							<li><Icon type="dropbox" /><span>饼干/膨化</span><Icon type="right" /></li>
+							<li><Icon type="dropbox" /><span>饼干/膨化</span><Icon type="right" /></li>
+						</ul>
 					</ul>
 				</div>
 
@@ -36,7 +45,14 @@ class Home extends React.Component{
 					    <div><img src="http://demo.cssmoban.com/cssthemes4/amz_02_adp/two/images/ad7.jpg" /></div>
 					    <div><img src="http://demo.cssmoban.com/cssthemes4/amz_02_adp/two/images/ad8.jpg" /></div>
 					    <div><img src="http://demo.cssmoban.com/cssthemes4/amz_02_adp/two/images/ad5.jpg" /></div>
-					  </Carousel>
+					</Carousel>
+
+					<div id="swiper_bot">
+						<h3>商城头条</h3>
+						<Button>登录</Button>
+						<Button>注册</Button>
+					</div>
+
 				</div>
 
 
